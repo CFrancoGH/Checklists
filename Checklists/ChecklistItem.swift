0f7573cf7e59119bfe13469420c81090a40eca51
Boondoggle <email.cfranco@gmail.com>
2021-08-29 21:50:59 -0400

    Checklists

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,13 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Christopher Franco on 8/28/21.
+//
+
+import Foundation
+
+class ChecklistItem: NSObject {
+    var text = ""
+    var checked = false
+}
